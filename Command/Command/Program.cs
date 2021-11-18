c7b49f5f1ab3ab3d3fe63ddd45ec7b5fdbb80a62
Rafael <rafaelsantos.rsdn@gmail.com>
2021-11-18 14:48:53 -0300

    First Commit

---
+++ Command/Command/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesignPatterns;
+using System;
+
+namespace Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecucaoCommand.Executar();
+        }
+    }
+}
